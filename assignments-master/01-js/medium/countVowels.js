1973565721029a33ed70962c74e4cc39651e144f
Vansh Dhar <11avanshdhar@gmail.com>
2024-02-18 03:30:12 +0530

    vowels

--- assignments-master/01-js/medium/countVowels.js
+++ assignments-master/01-js/medium/countVowels.js
@@ -7,6 +7,26 @@
 
 function countVowels(str) {
     // Your code here
+    let strCopy = str.toLowerCase()
+  console.log(strCopy)
+    let count = 0;
+
+    if (!strCopy.length){
+      return 0;
+    }
+
+    for (let i = 0; i < strCopy.length; i++){
+      if (strCopy[i] === 'a' || strCopy[i] === 'e' || strCopy[i] === 'i' || strCopy[i] ==='o' ||strCopy[i] === 'u'){
+        console.log(strCopy[i])
+
+        count+=1;
+
+      }
+    }
+    return count;
+
 }
 
+res = countVowels('Hello, world!')
+console.log(res)
 module.exports = countVowels;
