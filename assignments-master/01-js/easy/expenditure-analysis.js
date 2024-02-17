7175fa4d5ac64894a1967e254e44da75fff2f3e2
Vansh Dhar <11avanshdhar@gmail.com>
2024-02-18 03:19:53 +0530

    expenditure

--- assignments-master/01-js/easy/expenditure-analysis.js
+++ assignments-master/01-js/easy/expenditure-analysis.js
@@ -14,12 +14,59 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
+  let productArray = [];
+  if(!transactions.length){
+    return []
+  }
 
-  const output = [];
+  const output = []; // final list to be returned
+  output.push({
+    category: transactions[0]["category"], 
+    totalSpent: transactions[0]["price"]
+  })
+  console.log(output)
+  productArray.push(transactions[0]["category"])
+
+  for (let i = 1; i < transactions.length; i++){
+
+    let productCategory = transactions[i]["category"]
+    console.log("Product category ->", productCategory)
+    // let outputCategory = output[j]["category"]
+
+    for (let j = 0; j <= output.length; j++){
+
+
+
+      if (j == output.length){
+        console.log("1st if executed")
+        output.push({
+          category: transactions[i]["category"], 
+          totalSpent: transactions[i]["price"]
+        })
+        break;
+      }
+
+
+      if (productCategory != output[j]["category"]){
+        console.log("2nd if executeed")
+        // output.push({
+        //   category: transactions[i]["category"],
+        //   totalSpent: transactions[i]["price"]
+        // })
+        continue;
+      }
+
+      else{
+        console.log("3rd if executed")
+        output[j]["totalSpent"] += transactions[i]["price"];
+        break;
+      }
 
   
 
-  return [];
-}
+    }
 
+  }
+    return output;
+  }
 module.exports = calculateTotalSpentByCategory;
